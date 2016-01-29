e1adc9e876bae00cbcdf810c97856174872eb7f2
Stanislav Zhurich <stanislav.zhurich@gmail.com>
2016-01-29 23:05:36 +0300

    gradle changes

--- src/main/scala/com/stan/meetapp/services/common/api/ActorCreationSupport.scala
+++ src/main/scala/com/stan/meetapp/services/common/api/ActorCreationSupport.scala
@@ -2,14 +2,40 @@ package com.stan.meetapp.services.common.api
 
 import akka.actor.ActorContext
 import akka.actor.Props
+import akka.actor.ActorRef
+import akka.actor.Actor
 
 /**
  * 
+ * Exposes methods for actor creation
+ * 
  * @author Stanislav Zhurich
+ * 
  */
-trait ActorCreationSupport {
+trait ActorCreationSupport { this:Actor => 
   
   def context:ActorContext
   
-  def createChild(props:Props, name:String) = context.actorOf(props, name)
+  /**
+   * method for creating an actor.
+   *
+   * @param 	props  properties to be used for new actor
+   * @param		name   of the actor 
+   * @return       the newly created actor.
+   */
+  def createChild(props:Props, name:String):ActorRef = context.actorOf(props, name)
+  
+ /**
+   * method for creating an actor.
+   *
+   * @param 	class  actor class to be created
+   * @param		name   of the actor 
+   * @param		args   optional list of the arguments to be passed in actor constructor
+   * @return       the newly created actor.
+   */
+  def createChild(clazz:Class[_], name:String, args:Any*):ActorRef = {
+    val props = Props.apply(clazz, args)
+    context.actorOf(props, name)
+  }
+  
 }
